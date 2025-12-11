1e3c8a2f6ffa78f1322625a6835e0779989883d8
Marqui <bmay86@protonmail.com>
2025-12-10 22:00:57 -0500

    Implemented supporting logic in script.js to generate two random color values, update both color inputs, and apply a new gradient on click. Also wired the button to trigger the random gradient function.

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,16 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomBtn = document.getElementById("random");
+
+function randomColor() {
+  return (
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")
+  );
+}
 
 function initialLoad() {
   body.style.background =
@@ -17,5 +27,12 @@ function setGradient() {
   css.textContent = body.style.background + ";";
 }
 
+function randomGradient() {
+  color1.value = randomColor();
+  color2.value = randomColor();
+  setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+randomBtn.addEventListener("click", randomGradient);
